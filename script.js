2cea789b208b2e1aeac2b905084f1aea67d43538
abhijitsarkar2108-collab <abhijitsarkar2108@gmail.com>
2026-08-13 23:32:02 +0530

    Update alert message for adding item to cart

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ let cartCount = 0;
 function addToCart() {
     cartCount++;
     document.getElementById('cart-count').innerText = cartCount;
-    alert("Item added to your cart!");
+    
+    // Optional: Show a small alert
+    alert('Product added to your cart!');
 }
